c4f94564d582e03fc456e02f42a4af564aa3ec89
Ondrej Cvacho <ondrej.cvacho@gmail.com>
2020-01-30 02:25:31 +0100

    improved random eds generator

--- scripts/random_eds.py
+++ scripts/random_eds.py
@@ -1,54 +1,78 @@
 from optparse import OptionParser
 import random
+import math
 
 DNA_ALPHABET = 'ACGT'
 PROTEIN_ALPHABET = 'ACDEFGHIKLMNPQRSTVWY'
 
+
+class AverageSettings:
+    def __init__(self, length, length_dev, number, number_dev):
+        self.length = length
+        self.length_dev = length_dev
+        self.number = number
+        self.number_dev = number_dev
+
+    @staticmethod
+    def create_reduced_copy(settings):
+        return AverageSettings(math.log2(settings.length), settings.length_dev, math.log2(settings.number), settings.number_dev)
+    
+    def get_number(self):
+        return int(math.fabs(random.gauss(self.number, self.number_dev)))
+    
+    def get_length(self, max_length=None):
+        rnd_length = int(math.fabs(random.gauss(options.average, options.deviation)))
+        if max_length is not None and rnd_length > max_length:
+            return max_length
+        return rnd_length
+
+
 def get_random_string(alphabet, length):
     return ''.join([random.choice(alphabet) for _ in range(length)])
 
-def get_random_string_length(options):
-    return int(random.gauss(options.average, options.deviation))
 
-
-def get_degenerate_segment(options, alphabet, curr_depth):
-    print("depth {}".format(curr_depth))
+def get_degenerate_segment(options, alphabet, curr_depth, avg_settings, max_length=None):
     if curr_depth == options.max_reds_depth:
-        return [get_random_string(alphabet, get_random_string_length(options))]
+        return [get_random_string(alphabet, avg_settings.get_length(max_length))]
 
     degenerate_segment = []
 
-    for _ in range(int(random.gauss(options.number, options.number_deviation))):
-        segment_length = get_random_string_length(options)
+    number_of_items = avg_settings.get_number()
+    for _ in range(number_of_items):
+        segment_length = avg_settings.get_length(max_length)
 
         segment_str = ''
         i = 0
         while i < segment_length:
             if random.random() < options.reds_prob:
-                # degenerate recursive segment
-                degenerate_segment = get_degenerate_segment(options, alphabet, curr_depth + 1)
+                data = get_degenerate_segment(options, alphabet, curr_depth + 1,
+                                                AverageSettings.create_reduced_copy(avg_settings),
+                                                segment_length)
+                if len(data) == 1:
+                    segment_str += data[0]
+                else:
+                    segment_str += '{' + ','.join(data) + '}'
 
-                segment_str += '{' + ','.join(degenerate_segment) + '}'
-                i += len(degenerate_segment[0])
+                i += len(data[0])
             else:
-                # simple segment
                 segment_str += random.choice(alphabet)
                 i += 1
 
         degenerate_segment.append(segment_str)
 
     if len(degenerate_segment) == 0:
-        degenerate_segment.append(get_random_string(alphabet, get_random_string_length(options)))
+        degenerate_segment.append(get_random_string(alphabet, avg_settings.get_length(max_length)))
     
     return degenerate_segment
+
 
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option('-a', '--alphabet', dest='alphabet',
                     help="Alphabet type (valid values: D for DNA, P for Protein")
-    parser.add_option('-p', '--probability', dest="probability", default=0.18, metavar='NUMBER',
+    parser.add_option('-p', '--probability', dest="probability", default=0.15, metavar='NUMBER',
                     type='float', help="Probability of degenerate segment on each position")
-    parser.add_option('-e', '--average', dest="average", default=4, metavar='NUMBER',
+    parser.add_option('-e', '--average', dest="average", default=5, metavar='NUMBER',
                     type='int', help="Gaussian distribution mean of the length of degenerate segment")
     parser.add_option('-d', '--deviation', dest="deviation", default=0.5, metavar='NUMBER',
                     type='float', help="Gaussian distribution standard deviation of the length of degenerate segment")
@@ -56,18 +80,18 @@ if __name__ == "__main__":
                     type='int', help="Gaussian distribution mean of the number of string in degenerate segment")
     parser.add_option('-m', '--ndeviation', dest="number_deviation", default=0.5, metavar='NUMBER',
                     type='float', help="Gaussian distribution standard deviation of the number of string in degenerate segment")
-    parser.add_option('-r', '--recursive', dest="reds_prob", default=0.1, metavar='NUMBER',
+    parser.add_option('-r', '--recursive', dest="reds_prob", default=0.05, metavar='NUMBER',
                     type='float', help="Probability of starting recursive segment")
     parser.add_option('-b', '--maxdepth', dest="max_reds_depth", default=5, metavar='NUMBER',
                     type='int', help="Maximum depth of recursive segments")
     parser.add_option('-l', '--length', dest="length", default=1000, metavar='NUMBER',
-                    type='int', help="Length of degenerate segment")
+                    type='int', help="Length of EDS - degenerate segments add to length with its first element")
     parser.add_option('-o', '--output', dest="output", metavar='FILE', default='output.eds',
                     help='Store generated file to given file')
 
     (options, args) = parser.parse_args()
 
-    print('Random EDS string generator 0.1.0')
+    print('Random EDS string generator 0.5')
 
     alphabet = DNA_ALPHABET
     if options.alphabet == 'P':
@@ -92,9 +116,14 @@ if __name__ == "__main__":
     while i < options.length:
         if random.random() < options.probability:
             # degenerate segment
-            degenerate_segment = get_degenerate_segment(options, alphabet, 0)
+            avg_settings = AverageSettings(options.average, options.deviation, options.number, options.number_deviation)
+            degenerate_segment = get_degenerate_segment(options, alphabet, 0, avg_settings)
 
-            eds_str += '{' + ','.join(degenerate_segment) + '}'
+            if len(degenerate_segment) == 1:
+                eds_str += degenerate_segment[0]
+            else:
+                eds_str += '{' + ','.join(degenerate_segment) + '}'
+
             i += len(degenerate_segment[0])
         else:
             # simple segment
